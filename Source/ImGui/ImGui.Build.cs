c798b3418429ded789a6e4d66d85f2dec5ab6dbf
Sammy Fatnassi <sammyfreg@gmail.com>
2021-07-16 21:40:36 +0900

    Added a define programmer can rely on to know if UnrealImgui is active

--- Source/ImGui/ImGui.Build.cs
+++ Source/ImGui/ImGui.Build.cs
@@ -22,6 +22,7 @@ public class ImGui : ModuleRules
 		// Developer modules are automatically loaded only in editor builds but can be stripped out from other builds.
 		// Enable runtime loader, if you want this module to be automatically loaded in runtime builds (monolithic).
 		bool bEnableRuntimeLoader = true;
+		bool bUnrealImguiEnabled = true;
 
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
@@ -96,7 +97,7 @@ public class ImGui : ModuleRules
 #if !UE_4_19_OR_LATER
 		List<string> PrivateDefinitions = Definitions;
 #endif
-
+		PublicDefinitions.Add(string.Format("UNREAL_IMGUI_ENABLED={0}", bUnrealImguiEnabled ? 1 : 0));
 		PrivateDefinitions.Add(string.Format("RUNTIME_LOADER_ENABLED={0}", bEnableRuntimeLoader ? 1 : 0));
 	}
 }
