cff6ff6f669428c464bae8e1f304c7b927eae59f
Steve Streeting <steve@stevestreeting.com>
2023-02-16 17:05:16 +0000

    Fix compilation error on UE 5.1

--- Source/ImGui/Private/ImGuiImplementation.cpp
+++ Source/ImGui/Private/ImGuiImplementation.cpp
@@ -6,7 +6,10 @@
 
 // For convenience and easy access to the ImGui source code, we build it as part of this module.
 // We don't need to define IMGUI_API manually because it is already done for this module.
-
+// UE 5.1 stopped defining PLATFORM_XBOXONE, so be safe if not defined
+#if !defined(PLATFORM_XBOXONE)
+	#define PLATFORM_XBOXONE 0
+#endif
 #if PLATFORM_XBOXONE
 // Disable Win32 functions used in ImGui and not supported on XBox.
 #define IMGUI_DISABLE_WIN32_DEFAULT_CLIPBOARD_FUNCTIONS
