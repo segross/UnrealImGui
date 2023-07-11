5dc3b1f2220290f8b8d80601acd3f7c515ade659
Steve Streeting <steve@stevestreeting.com>
2023-07-11 16:30:58 +0100

    Remove "Handler Down" debug message

--- Source/ImGui/Private/ImGuiInputHandler.cpp
+++ Source/ImGui/Private/ImGuiInputHandler.cpp
@@ -135,7 +135,7 @@ FReply UImGuiInputHandler::OnMouseButtonDown(const FPointerEvent& MouseEvent)
 		FImGuiContextProxy* Proxy = ModuleManager->GetContextManager().GetContextProxy(0);
 		if (Proxy)
 		{
-			GEngine->AddOnScreenDebugMessage(15, 10, Proxy->WantsMouseCapture() ? FColor::Green : FColor::Red, TEXT("Handler Down"));
+			//GEngine->AddOnScreenDebugMessage(15, 10, Proxy->WantsMouseCapture() ? FColor::Green : FColor::Red, TEXT("Handler Down"));
 			return ToReply(Proxy->WantsMouseCapture());
 		}
 	}
